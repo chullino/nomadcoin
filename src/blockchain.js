8be9a5fad64bbe437455b42829ca87ff1af1a947
chullino <chullino93@gmail.com>
2018-07-08 20:37:36 +0900

    lecture 1

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,3 +1,5 @@
+const CryptoJS = require("crypto-js")
+
 class Block {
     constructor(index, hash, previousHash, timestamp, data){
         this.index = index
@@ -10,7 +12,7 @@ class Block {
 
 const genesisBlock = new Block(
 			 0,
-    			 "hashed value",
+    			 "sha-256 hashed result of genesis block",
     			 null,
     			 new Date().getTime()/1000,
 			 "The Genesis Block")
@@ -19,10 +21,99 @@ let blockchain = [genesisBlock]
 
 const getLastBlock = () => blockchain[blockchain.length - 1]
 
-const getTimestamp = () => new Date().getTime() / 1000;
+const getTimestamp = () => new Date().getTime() / 1000
+
+const getBlockchain = () => blockchain
+
+const createHash = (index, previousHash, timestamp, data) =>
+    CryptoJS.SHA256(index + previousHash, timestamp + JSON.stringify(data))
+		.toString()
+
+const getBlockHash = (block) => createHash(block.index, block.previousHash, block.timestamp, block.data)
 
 const createNewBlock = data => {
-    const previousBlock = getLastBlock();
-    const newBlockIndex = previousBlock.index + 1;
-    const newTimestamp = getTimestamp();
+    const previousBlock = getLastBlock()
+    const newBlockIndex = previousBlock.index + 1
+    const newTimestamp = getTimestamp()
+    const newHash = createHash(newBlockIndex, 
+			       previousBlock.hash, 
+	    		       newTimestamp,
+    			       data)
+    const newBlock = new Block(newBlockIndex,
+    			       newHash,
+    			       previousBlock.hash,
+    			       newTimestamp,
+    			       data)
+    addBlockToChain(newBlock)
+    return newBlock
+}
+
+const isNewBlockValid = (candidateBlock, latestBlock) => {
+    if(!isNewStructureValid(candidateBlock)){
+	console.log("The candidate block structure is not valid")
+	return false
+    }
+    else if(latestBlock.index + 1 !== candidateBlock.index){
+        console.log('The candidate block does not have a valid index.')
+	return false
+    }
+    else if(latestBlock.hash !== candidateBlock.previousHash){
+	console.log('The previousHash of the candidate block is not the hash of the latest block.')
+	return false
+    }
+    else if(getBlockHash(candidateBlock) !== candidateBlock.hash){
+	console.log('The hash of this block is invalid.')
+	return false
+    }
+    return true
+}
+
+const isNewStructureValid = (block) => {
+    return (
+        typeof block.index === "number" &&
+	typeof block.hash === "string" &&
+	typeof block.previousHash === "string" &&
+	typeof block.timestamp === "number" &&
+	typeof block.data === "string"
+    )
+}
+
+const isChainValid = candidateChain => {
+    const isGenesisValid = block => {
+	return JSON.stringify(block) === JSON.stringify(genesisBlock)
+    }
+    if(!isGenesisValid(candidateChain[0])){
+	console.log("The candidateChains's genesisBlock is not the same as our genesisBlock")
+	return false;
+    }
+    for(let i=1; i < candidateChain.length; i++){
+	if(!isNewBlockValid(candidateChain[i], candidateChain[i-1])){
+	    return false
+	}
+    }
+    return true
+}
+
+
+const replaceChain = candidateChain => {
+    if(isChainValid(candidateChain) && candidateChain.length > getBlockchain().length){
+	blockchain = candidateChain
+	return true
+    } else{
+	return false
+    }
+}
+
+const addBlockToChain = newBlock => {
+    if(isNewBlockValid(newBlock, getLastBlock())){
+	getBlockchain().push(candidateBlock)
+	return true
+    } else {
+	return false
+    }
+}
+
+module.exports = {
+    getBlockchain,
+    createNewBlock
 }
---
+++ src/p2p.js
@@ -0,0 +1,27 @@
+const WebSockets = require("ws")
+
+const sockets = []
+
+const startP2PServer = server => {
+    const wsServer = new WebSockets.Server({server})
+    wsServer.on("connection", ws => {
+        console.log(`Hello ${ws}`)
+    })
+    console.log('Nomadcoin P2P Server Running!')
+}
+
+const initSocketConnection = socket => {
+    sockets.push(socket)
+}
+
+const connectToPeers = newPeer => {
+    const ws = new WebSockets(newPeer)
+    ws.on("open",() => {
+        initSocketConnection(ws)
+    })
+}
+
+module.exports = {
+    startP2PServer,
+    connectToPeers
+}
